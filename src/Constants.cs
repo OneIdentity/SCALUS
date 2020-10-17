e398c6a92f0c54eec27c35e4800c9a75e580db1e
Cody Batt <cody.batt@quest.com>
2020-10-17 17:39:34 -0600

    Register with dotnet + assembly

--- src/Constants.cs
+++ src/Constants.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Sulu
 {
     static class Constants
     {
+        public static string DotNetPath()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return @"C:\Program Files\dotnet\dotnet.exe";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return "/usr/bin/dotnet";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return "/usr/local/share/dotnet/dotnet";
+            }
+            return null;
+        }
+
         public static string GetBinaryPath()
         {
             return Environment.GetCommandLineArgs().First();
@@ -17,7 +35,16 @@ namespace Sulu
 
         public static string GetLaunchCommand(string urlString = "<URL VARIABLE HERE>")
         {
-            return $"\"{GetBinaryPath()}\" launch -u \"{urlString}\"";
+            var binPath = GetBinaryPath().Trim();
+            if (binPath.EndsWith(".dll", StringComparison.OrdinalIgnoreCase) || binPath.EndsWith(".so", StringComparison.OrdinalIgnoreCase))
+            {
+                binPath = $"\"{DotNetPath()}\" \"{binPath}\"";
+            }
+            else
+            {
+                binPath = $"\"{binPath}\"";
+            }
+            return $"{binPath} launch -u \"{urlString}\"";
         }
     }
 }
